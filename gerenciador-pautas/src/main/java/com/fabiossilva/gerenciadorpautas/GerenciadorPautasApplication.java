43c05a666335773c86e183c76b0cea7041e7cd40
Fábio <fabiossilva00@gmail.com>
2023-05-12 17:39:44 -0300

    init

---
+++ gerenciador-pautas/src/main/java/com/fabiossilva/gerenciadorpautas/GerenciadorPautasApplication.java
@@ -0,0 +1,13 @@
+package com.fabiossilva.gerenciadorpautas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorPautasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorPautasApplication.class, args);
+	}
+
+}
---
+++ gerenciador-pautas/src/test/java/com/fabiossilva/gerenciadorpautas/GerenciadorPautasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiossilva.gerenciadorpautas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPautasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
